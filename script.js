2731404a47874402d442cf2f77691d766c8f4ea8
Jafet Guzman <jafetguzmanflores@gmail.com>
2023-04-16 03:04:41 -0400

    made number list fn

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("hello world");
+function makeNumber(end) {
+    if (end < 1) {
+        throw Error("you can't have a negative list of integers");
+    }
+    if (end > 10000000) {
+        throw Error("end is toooo biggggggg");
+    }
+    //allocate the necessary memory once
+    var tempArr = new Array(end);
+    for (var index = 0; index < tempArr.length; index++) {
+        tempArr[index] = index;
+    }
+    return tempArr;
+}
+makeNumber(10000000);
+console.log("over");
--- script.ts
+++ script.ts
@@ -1,1 +1,17 @@
 console.log(`hello world`)
+
+
+type NumberArr=Array<number>
+
+function makeNumber(end:number):NumberArr{
+    if(end<1){throw Error(`you can't have a negative list of integers`)}
+    if(end>10000000){throw Error(`end is toooo biggggggg`)}
+    //allocate the necessary memory at start to avoid gc land
+    let tempArr=new Array(end)
+    for (let index = 0; index < tempArr.length; index++) {
+      tempArr[index]=index
+    }
+    return tempArr
+
+}
+
