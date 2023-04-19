40736bf6999a1583f58c0e5273f8b19923ec1654
Jafet Guzman <jafetguzmanflores@gmail.com>
2023-04-18 23:12:36 -0400

    made makeNumber a dynamic fn and made .gitignore
    .gitignore wont save any file starting with todo and my fn is now
    dynamic

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 console.log("hello world");
-function makeNumber(end) {
+//for this function, assume the expected outcome to be(start,end+1]
+//while the input (from user) is (start,end)
+function makeNumber(start, end) {
     if (end < 1) {
         throw Error("you can't have a negative list of integers");
     }
     if (end > 10000000) {
         throw Error("end is toooo biggggggg");
     }
-    //allocate the necessary memory once
-    var tempArr = new Array(end);
-    for (var index = 0; index < tempArr.length; index++) {
-        tempArr[index] = index;
+    if (start > end) {
+        throw Error("start can't be bigger  than, or equal to, the end");
+    }
+    // change end to an exclusive while keeping the input as (start,end) by adding one
+    end++;
+    //allocate the necessary memory at start to avoid gc land
+    var tempArr = Array(end - start);
+    for (var index = 0; index + start <= end; index++) {
+        tempArr[index] = index + start;
     }
     return tempArr;
 }
-makeNumber(10000000);
-console.log("over");
--- script.ts
+++ script.ts
@@ -1,17 +1,19 @@
 console.log(`hello world`)
 
-
 type NumberArr=Array<number>
 
-function makeNumber(end:number):NumberArr{
+//for this function, assume the expected outcome to be(start,end+1]
+//while the input (from user) is (start,end)
+function makeNumber(start:number, end:number):NumberArr{
     if(end<1){throw Error(`you can't have a negative list of integers`)}
     if(end>10000000){throw Error(`end is toooo biggggggg`)}
+  if(start>end){throw Error(`start can't be bigger  than, or equal to, the end`)}
+// change end to an exclusive while keeping the input as (start,end) by adding one
+  end++
     //allocate the necessary memory at start to avoid gc land
-    let tempArr=new Array(end)
-    for (let index = 0; index < tempArr.length; index++) {
-      tempArr[index]=index
+    let tempArr= Array(end-start)
+    for (let index = 0; index+start <= end; index++) {
+      tempArr[index]=index+start
     }
     return tempArr
-
 }
-
